b53c1080d1ed76737e68d927a5bdf676f9e4fc9b
KKTRKKT <paulhana6006@gmail.com>
2023-01-26 22:35:45 +0900

    Optional 추가

--- src/main/java/me/kktrkkt/java8to11/optional/App.java
+++ src/main/java/me/kktrkkt/java8to11/optional/App.java
@@ -2,6 +2,8 @@ package me.kktrkkt.java8to11.optional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
 
 public class App {
 
@@ -14,10 +16,18 @@ public class App {
         springClasses.add(new OnlineClass(5, "rest api development", false));
 
         OnlineClass springBoot = springClasses.get(0);
-        Progress progress = springBoot.getProgress();
-        //기존 null 처리 방법
-        if(progress != null){
-            System.out.println(progress.getStudyDuration());
-        }
+//        Progress progress = springBoot.getProgress();
+//        //기존 null 처리 방법
+//        if(progress != null){
+//            System.out.println(progress.getStudyDuration());
+//        }
+
+        //Optional은 리턴값으로만 사용
+        Optional<Progress> progress = springBoot.getProgress();
+        progress.ifPresent(p-> System.out.println(p.getStudyDuration()));
+
+        //기본형은 전용 Optinoal 사용.
+        OptionalInt optionalInt = OptionalInt.of(10);
+        optionalInt.ifPresent(System.out::println);
     }
 }
--- src/main/java/me/kktrkkt/java8to11/optional/OnlineClass.java
+++ src/main/java/me/kktrkkt/java8to11/optional/OnlineClass.java
@@ -1,5 +1,7 @@
 package me.kktrkkt.java8to11.optional;
 
+import java.util.Optional;
+
 public class OnlineClass {
 
     private Integer id;
@@ -9,6 +11,8 @@ public class OnlineClass {
     private boolean closed;
 
     private Progress progress;
+
+//    private Optional<Progress> progress; X progrss 자체가 null일 수 있음
 
     public OnlineClass(Integer id, String title, boolean closed) {
         this.id = id;
@@ -40,10 +44,13 @@ public class OnlineClass {
         this.closed = closed;
     }
 
-    public Progress getProgress() {
-        return progress;
+    public Optional<Progress> getProgress() {
+//        return null X 클라이언트측에서 null 체크를
+//        return Optional.empty() O null 대신 리턴
+        return Optional.ofNullable(progress);
     }
 
+//    public void setProgress(Optional<Progress> progress) X 클라이언트에서 null을 던질 수 있음
     public void setProgress(Progress progress) {
         this.progress = progress;
     }
