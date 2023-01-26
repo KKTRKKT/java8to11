dea9363b5b4121c7bd39210c7e53654597f099b0
KKTRKKT <paulhana6006@gmail.com>
2023-01-26 23:35:10 +0900

    map 설명 수정

--- src/main/java/me/kktrkkt/java8to11/optional/App.java
+++ src/main/java/me/kktrkkt/java8to11/optional/App.java
@@ -63,7 +63,7 @@ public class App {
         System.out.println("---------------map--------------------");
 
         //map 값이 있으면 매핑함수를 적용한 값을 Optional에 실어서 반환
-        //매핑함수 적용값이 Optional일 경우 Optioanl<Optaionl>이 될 수 있어 flatMap을 사용
+        //만약 매핑함수 적용 반환값이 Optional일 경우 Optioanl<Optaionl<T>>이 될 수 있어 flatMap을 사용
         optional.map(OnlineClass::getId).ifPresent(System.out::println);
 //        Optional<Optional<Progress>> progress = optional.map(OnlineClass::getProgress);
 
