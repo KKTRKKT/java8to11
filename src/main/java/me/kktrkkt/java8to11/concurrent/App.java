d576d2fefade603c8aa109dfd96b67a2b8ca1916
KKTRKKT <paulhana6006@gmail.com>
2023-01-29 16:51:22 +0900

    멀티스레드 생성방법2 추가

--- src/main/java/me/kktrkkt/java8to11/concurrent/App.java
+++ src/main/java/me/kktrkkt/java8to11/concurrent/App.java
@@ -7,17 +7,28 @@ public class App {
         Thread thread = new MyThread();
         thread.start();
 
+        // Runnable 인터페이스를 구현해서 생성
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("annonymous:"+Thread.currentThread().getName());
+            }
+        });
+        thread1.start();
 
+        //자바 8부터는 람다로 간단하게 생성 가능
+        Thread thread2 = new Thread(()-> System.out.println("lambda:"+Thread.currentThread().getName()));
+        thread2.start();
 
         //메인 스레드
-        System.out.println(Thread.currentThread().getName());
+        System.out.println("main:"+Thread.currentThread().getName());
 
     }
 
     private static class MyThread extends Thread {
         @Override
         public void run() {
-            System.out.println("hello:" + this.getName());
+            System.out.println("extends:" + this.getName());
         }
     }
 }
