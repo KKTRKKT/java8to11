a91cebd4d9194923dd9b0403c0807f5e48c5650d
KKTRKKT <paulhana6006@gmail.com>
2023-01-29 16:40:08 +0900

    메인스레드 확인 추가

---
+++ src/main/java/me/kktrkkt/java8to11/concurrent/App.java
@@ -0,0 +1,9 @@
+package me.kktrkkt.java8to11.concurrent;
+
+public class App {
+
+    public static void main(String[] args) {
+        //메인 스레드
+        System.out.println(Thread.currentThread().getName());
+    }
+}
