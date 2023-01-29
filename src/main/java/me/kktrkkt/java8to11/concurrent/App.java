7dad9c8e5c8fa2099c6111edd0ba04ed17ec5c17
KKTRKKT <paulhana6006@gmail.com>
2023-01-29 16:46:12 +0900

    멀티스레드 생성 방법1 추가

--- src/main/java/me/kktrkkt/java8to11/concurrent/App.java
+++ src/main/java/me/kktrkkt/java8to11/concurrent/App.java
@@ -3,7 +3,21 @@ package me.kktrkkt.java8to11.concurrent;
 public class App {
 
     public static void main(String[] args) {
+        //멀티스레드에서 스레드의 순서는 보장하지 못한다.
+        Thread thread = new MyThread();
+        thread.start();
+
+
+
         //메인 스레드
         System.out.println(Thread.currentThread().getName());
+
+    }
+
+    private static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("hello:" + this.getName());
+        }
     }
 }
