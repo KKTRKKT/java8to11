157f4e6d5e8d9b60dad541a0d15ab8961741286c
KKTRKKT <paulhana6006@gmail.com>
2023-01-31 21:52:24 +0900

    ScheduledExecutorService 추가

--- src/main/java/me/kktrkkt/java8to11/excutors/App.java
+++ src/main/java/me/kktrkkt/java8to11/excutors/App.java
@@ -2,10 +2,13 @@ package me.kktrkkt.java8to11.excutors;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class App {
 
     public static void main(String[] args) {
+        System.out.println("---------------------ExecutorService--------------------------");
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         executorService.submit(getRunnable("Hello "));
         //종료를 명시해야됨
@@ -22,6 +25,16 @@ public class App {
         executorService2.submit(getRunnable("fourth "));
         executorService2.shutdown();
 
+        System.out.println("---------------------ScheduledExecutorService--------------------------");
+
+        // 일정에 맞춰서 스레드 실행
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        // 3초뒤에 실행
+        scheduledExecutorService.schedule(getRunnable("schedule "), 3, TimeUnit.SECONDS);
+        // 3초뒤에 실행후 2초마다 실행
+        scheduledExecutorService.scheduleAtFixedRate(getRunnable("rateSchedule "), 3, 2, TimeUnit.SECONDS);
+        // 6초후에 종료
+        scheduledExecutorService.schedule(scheduledExecutorService::shutdown, 6, TimeUnit.SECONDS);
     }
 
     private static Runnable getRunnable(String message) {
