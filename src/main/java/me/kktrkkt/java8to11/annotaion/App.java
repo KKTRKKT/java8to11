862052fcdfbbfa652190c3dd6d12ec81c4d5f164
KKTRKKT <paulhana6006@gmail.com>
2023-02-05 17:56:26 +0900

    TYPE_USE 추가

--- src/main/java/me/kktrkkt/java8to11/annotaion/App.java
+++ src/main/java/me/kktrkkt/java8to11/annotaion/App.java
@@ -1,12 +1,18 @@
 package me.kktrkkt.java8to11.annotaion;
 
+import java.util.List;
+
+@TypeUse
 public class App {
 
-    public static void main(String[] args) {
-
+    // void 타입에는 사용할 수 없다
+//    @TypeUse
+    public static void main(@TypeUse String[] args) {
+        List<@TypeUse String> names = List.of("name");
     }
-    
-    static private class Chicken<@TypeParameter T> {
+
+    @TypeUse
+    static private class Chicken<@TypeUse @TypeParameter T> {
 
     }
 }
---
+++ src/main/java/me/kktrkkt/java8to11/annotaion/TypeUse.java
@@ -0,0 +1,12 @@
+package me.kktrkkt.java8to11.annotaion;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+// 타입을 사용하는 모든 곳에서 사용할 수 있다.
+@Target(ElementType.TYPE_USE)
+public @interface TypeUse {
+}
