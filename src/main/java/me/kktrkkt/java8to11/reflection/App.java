81ed2b6d51d93a39233bdaf969d3613495f3e46d
KKTRKKT <paulhana6006@gmail.com>
2023-02-16 22:41:52 +0900

    class 가져오기 추가

--- src/main/java/me/kktrkkt/java8to11/reflection/App.java
+++ src/main/java/me/kktrkkt/java8to11/reflection/App.java
@@ -4,5 +4,17 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) throws ClassNotFoundException {
+        // 클래스 로딩이 끝나면 클래스 타입의 인스턴스를 만들어서 힙에 저장됨
+        System.out.println("--------------------------class 가져오기-------------------------------");
+        // 타입에서 가져오는 방법
+        Class<Book> bookClass = Book.class;
+
+        // 인스턴스에서 가져오는 방법
+        Book book = new Book();
+        Class<? extends Book> aClass = book.getClass();
+
+        // FQCN(Full Qualified Class Name)으로 찾는 방법
+        Class<?> forName = Class.forName("me.kktrkkt.java8to11.reflection.Book");
+
     }
 }
