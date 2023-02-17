1293ecb4b0c9de4884adb65046977a45cd49b8da
KKTRKKT <paulhana6006@gmail.com>
2023-02-17 11:10:39 +0900

    Retention 추가

--- src/main/java/me/kktrkkt/java8to11/reflection/MyAnnotation.java
+++ src/main/java/me/kktrkkt/java8to11/reflection/MyAnnotation.java
@@ -1,3 +1,6 @@
 package me.kktrkkt.java8to11.reflection;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnnotation {
 }
