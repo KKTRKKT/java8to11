156d6c2058f477d17c48d2f65923f7631ce4dd13
KKTRKKT <paulhana6006@gmail.com>
2023-02-22 14:28:47 +0900

    특정 메소드만 적용 추가

--- src/test/java/me/kktrkkt/java8to11/dynamicProxy/BookServiceTest.java
+++ src/test/java/me/kktrkkt/java8to11/dynamicProxy/BookServiceTest.java
@@ -14,6 +14,11 @@ class BookServiceTest {
                 BookService bookService = new BookServiceImpl();
                 @Override
                 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    // 특정 메소드만 적용하고 싶다면 조건문을 추가해야한다.
+                    // rent 메소드만 앞뒤로 출력을 덧붙이도록 한다
+                    if(!method.getName().equals("rent")) {
+                        return method.invoke(bookService, args);
+                    }
                     System.out.println("aaa");
                     Object invoke = method.invoke(bookService, args);
                     System.out.println("bbb");
@@ -26,5 +31,6 @@ class BookServiceTest {
         Book book = new Book();
         book.setTitle("spring");
         bookService.rent(book);
+        bookService.returnBook(book);
     }
 }
