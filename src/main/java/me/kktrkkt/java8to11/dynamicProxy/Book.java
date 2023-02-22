1cb36b14cdb03d6089ebeff2033620042873ea68
KKTRKKT <paulhana6006@gmail.com>
2023-02-22 14:20:53 +0900

    Book 추가

---
+++ src/main/java/me/kktrkkt/java8to11/dynamicProxy/Book.java
@@ -0,0 +1,13 @@
+package me.kktrkkt.java8to11.dynamicProxy;
+
+public class Book {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
