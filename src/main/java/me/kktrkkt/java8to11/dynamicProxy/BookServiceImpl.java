a6adf8b42a0a84c48e4c1db5da51735e28482332
KKTRKKT <paulhana6006@gmail.com>
2023-02-22 14:21:22 +0900

    BookServiceImpl 추가

---
+++ src/main/java/me/kktrkkt/java8to11/dynamicProxy/BookServiceImpl.java
@@ -0,0 +1,14 @@
+package me.kktrkkt.java8to11.dynamicProxy;
+
+public class BookServiceImpl implements BookService{
+
+    @Override
+    public void rent(Book book) {
+        System.out.println("rent: "+ book.getTitle());
+    }
+
+    @Override
+    public void returnBook(Book book) {
+
+    }
+}
