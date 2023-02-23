1f1d4f0c848f73c8a04eadc347688cff5a4894cd
KKTRKKT <paulhana6006@gmail.com>
2023-02-23 10:21:37 +0900

    Update BookServiceTest.java

--- src/test/java/me/kktrkkt/java8to11/dynamicProxy/BookServiceTest.java
+++ src/test/java/me/kktrkkt/java8to11/dynamicProxy/BookServiceTest.java
@@ -12,25 +12,26 @@ import java.lang.reflect.Proxy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookServiceTest {
-    BookService bookService = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[]{BookService.class},
-            new InvocationHandler() {
-                BookService bookService = new BookServiceImpl();
-                @Override
-                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    // 특정 메소드만 적용하고 싶다면 조건문을 추가해야한다.
-                    // rent 메소드만 앞뒤로 출력을 덧붙이도록 한다
-                    if(!method.getName().equals("rent")) {
-                        return method.invoke(bookService, args);
-                    }
-                    System.out.println("aaa");
-                    Object invoke = method.invoke(bookService, args);
-                    System.out.println("bbb");
-                    return invoke;
-                }
-            });
 
     @Test
     public void rent() {
+        BookService bookService = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[]{BookService.class},
+                new InvocationHandler() {
+                    final BookService bookService = new BookServiceImpl();
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        // 특정 메소드만 적용하고 싶다면 조건문을 추가해야한다.
+                        // rent 메소드만 앞뒤로 출력을 덧붙이도록 한다
+                        if(!method.getName().equals("rent")) {
+                            return method.invoke(bookService, args);
+                        }
+                        System.out.println("aaa");
+                        Object invoke = method.invoke(bookService, args);
+                        System.out.println("bbb");
+                        return invoke;
+                    }
+                });
+
         Book book = new Book();
         book.setTitle("spring");
         bookService.rent(book);
