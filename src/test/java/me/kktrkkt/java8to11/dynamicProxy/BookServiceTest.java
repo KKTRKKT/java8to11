82162965d1d2b4912f13335810b28bb780f17b52
KKTRKKT <paulhana6006@gmail.com>
2023-02-24 23:51:09 +0900

    rent_bytebuddy 추가

--- src/test/java/me/kktrkkt/java8to11/dynamicProxy/BookServiceTest.java
+++ src/test/java/me/kktrkkt/java8to11/dynamicProxy/BookServiceTest.java
@@ -1,14 +1,19 @@
 package me.kktrkkt.java8to11.dynamicProxy;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.InvocationHandlerAdapter;
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.nio.ByteBuffer;
 
+import static net.bytebuddy.matcher.ElementMatchers.named;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookServiceTest {
@@ -59,5 +64,33 @@ class BookServiceTest {
         book.setTitle("spring");
         cglibBookService.rent(book);
         cglibBookService.returnBook(book);
+    }
+
+    @Test
+    public void rent_bytebuddy() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<? extends BookServiceImpl> proxyClass = new ByteBuddy()
+                .subclass(BookServiceImpl.class)
+                .method(named("rent"))
+                .intercept(InvocationHandlerAdapter.of(new InvocationHandler() {
+                    final BookService bookService = new BookServiceImpl();
+
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        if (!method.getName().equals("rent")) {
+                            return method.invoke(bookService, args);
+                        }
+                        System.out.println("aaa");
+                        Object invoke = method.invoke(bookService, args);
+                        System.out.println("bbb");
+                        return invoke;
+                    }
+                }))
+                .make().load(BookServiceImpl.class.getClassLoader()).getLoaded();
+        BookService bookService = proxyClass.getConstructor(null).newInstance();
+
+        Book book = new Book();
+        book.setTitle("spring");
+        bookService.rent(book);
+        bookService.returnBook(book);
     }
 }
