fb7b5bb9b227c3bda74d29d78a3c40ee9d9312ac
KKTRKKT <paulhana6006@gmail.com>
2023-02-27 23:57:18 +0900

    Create App.java

---
+++ src/main/java/me/kktrkkt/java8to11/processor/App.java
@@ -0,0 +1,9 @@
+package me.kktrkkt.java8to11.processor;
+
+public class App {
+
+    public static void main(String[] args) {
+//        Moja moja = new MagicMoja();
+//        moja.pullOut();
+    }
+}
