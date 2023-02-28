888fa37a4a3c289657bb57f8f704b04f124ea40b
KKTRKKT <paulhana6006@gmail.com>
2023-02-28 21:42:42 +0900

    Update App.java

--- src/main/java/me/kktrkkt/java8to11/processor/App.java
+++ src/main/java/me/kktrkkt/java8to11/processor/App.java
@@ -3,7 +3,7 @@ package me.kktrkkt.java8to11.processor;
 public class App {
 
     public static void main(String[] args) {
-        MagicMoja moja = new MagicMoja();
+        Moja moja = new MagicMoja();
         System.out.println(moja.pullOut());
     }
 }
