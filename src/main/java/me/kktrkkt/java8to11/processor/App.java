de7e6ed22623197ff75519b17605df12ef0c973f
KKTRKKT <paulhana6006@gmail.com>
2023-02-28 23:08:51 +0900

    Update App.java

--- src/main/java/me/kktrkkt/java8to11/processor/App.java
+++ src/main/java/me/kktrkkt/java8to11/processor/App.java
@@ -3,6 +3,7 @@ package me.kktrkkt.java8to11.processor;
 public class App {
 
     public static void main(String[] args) {
+        // @Magic이 pullOut이 선언된 인터페이스에 붙어있을때, pullOut을 구현하는 MagicMoja 클래스를 제공한다.
         Moja moja = new MagicMoja();
         System.out.println(moja.pullOut());
     }
