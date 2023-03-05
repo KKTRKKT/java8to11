828a5fd15060c446f94dd25b1d118562657b6f67
KKTRKKT <paulhana6006@gmail.com>
2023-03-05 22:23:58 +0900

    SimpleArgumentConverter 테스트 추가

--- src/test/java/me/kktrkkt/java8to11/junit5/ParamTest.java
+++ src/test/java/me/kktrkkt/java8to11/junit5/ParamTest.java
@@ -1,9 +1,12 @@
 package me.kktrkkt.java8to11.junit5;
 
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ParamTest {
 
@@ -13,4 +16,17 @@ class ParamTest {
         System.out.println(value);
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void parameterized_test(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println(study);
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter{
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(targetType, Study.class, "Can only convert Study");
+            return new Study((Integer) source);
+        }
+    }
 }
