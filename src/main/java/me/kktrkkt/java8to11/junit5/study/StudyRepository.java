88113d2d84241eba267e2f6a31a932e74e538642
KKTRKKT <paulhana6006@gmail.com>
2023-03-08 10:25:08 +0900

    JPA 추가

--- src/main/java/me/kktrkkt/java8to11/junit5/study/StudyRepository.java
+++ src/main/java/me/kktrkkt/java8to11/junit5/study/StudyRepository.java
@@ -1,7 +1,7 @@
 package me.kktrkkt.java8to11.junit5.study;
 
 import me.kktrkkt.java8to11.junit5.domain.Study;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudyRepository {
-    Study save(Study study);
+public interface StudyRepository extends JpaRepository<Study, Long> {
 }
